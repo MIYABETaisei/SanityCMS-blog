3e0d21386b7d77c8c7317a226894d68982f6ef24
MIYABETaisei <mytspe46@gmail.com>
2022-06-26 23:23:05 +0900

    ♻️ リンクリストを配列へ格納

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import { FC } from "react";
 
+const NAV_ITEMS = [
+  { href: "/about", label: "About" },
+  { href: "/contact", label: "Contact" },
+];
+
 const Header: FC = () => {
   return (
     <header className="shadow-md shadow-black-500/40">
@@ -16,19 +21,24 @@ const Header: FC = () => {
             </a>
           </Link>
           <div className="hidden md:inline-flex items-center space-x-5">
-            <h3>About</h3>
-            <h3>Contact</h3>
+            {NAV_ITEMS.map((item) => {
+              return (
+                <Link href={item.href} key={item.label}>
+                  <a>{item.label}</a>
+                </Link>
+              );
+            })}
             <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
               Follow
             </h3>
           </div>
         </div>
-        <div className="flex items-center space-x-5 text-green-600">
+        {/* <div className="flex items-center space-x-5 text-green-600">
           <h3>Sign In</h3>
           <h3 className="border px-4 rounded-full border-green-600 py-1">
             Get Started
           </h3>
-        </div>
+        </div> */}
       </div>
     </header>
   );
